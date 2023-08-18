17d75e2b6b8f216614ec48c4df6112708d200648
furqan <furqan.azeem369@gmail.com>
2023-08-18 17:18:59 +0500

    latest code

---
+++ app.js
@@ -0,0 +1,499 @@
+              //chapter 1
+// Javascript assignment chapter 1 qno 1
+//alert("welcome to my web page");
+
+// Javascript assignment chapter 1 qno 2
+// alert("Error! Please Enter add Valid Password");
+
+// Javascript assignment chapter 1 qno 3
+// alert("Welcome to JS land..\n Happy Coding!")
+
+// Javascript assignment chapter 1 qno 4
+// alert("welcome to JS land");
+// alert("Happy Coding \n Prevent this page form creating additional dialogies");
+
+// Javascript assignment chapter 1 qno 5
+//completed
+
+// Javascript assignment chapter 1 qno 6 & 7
+// completed
+
+             //chapter 2
+// Javascript assignment chapter 2 qno 2
+// var myName = "Furqan Azeem";
+
+// Javascript assignment chapter 2 qno 3
+// var message = "Hello World";
+// alert(message);
+
+// Javascript assignment chapter 2 qno 4
+// var MyName = "Furqan Azeem";
+// var age =  17;
+// var edu = "SSUET"
+// var course = "cloud computing"
+// alert(MyName);
+// alert(age);
+// alert(edu);
+// alert(course);
+
+// Javascript assignment chapter 2 qno 5
+// var p = "pizza \n" + "pizz\n" + "piz\n" + "pi\n" + "p"
+// alert(p);
+
+// Javascript assignment chapter 2 qno 6
+// var job = "software developer";
+// var partnerName = "abc"
+// var child = "two";
+// var locat = "USA"
+// alert("you will be a "+job+" in "+locat+","+" and married to "+partnerName+" with "+child+" kids ") 
+
+// Javascript assignment chapter 2 qno 7
+// var email = "furi@gmail.com"
+// alert("my email address is "+email); 
+
+// Javascript assignment chapter 2 qno 8
+// var data = "A smarter way to learn JavaScript"
+// alert("i am trying to learn from the book " +"'"+data+"'")
+
+// Javascript assignment chapter 2 qno 9
+// var MyName = "My Name is Furqan Azeem";
+// var age = "my age is 17 years old" ;
+// var edu = "i studing in SSUET"
+// var course = "i take cloud computing"
+
+// Javascript assignment chapter 2 qno 10
+//completed
+ 
+                      //chapter 3
+
+ // Javascript assignment chapter 3 qno 1
+//   var age = 17;
+//   alert("i am "+age+" years old");
+
+// Javascript assignment chapter 3 qno 2
+    // var visit = 21;
+    // alert("You have visited this site " + visit + " times")
+
+// Javascript assignment chapter 3 qno 3
+// var birthYear = 2006;
+// document.write("my birth year is"+ birthYear);
+
+// Javascript assignment chapter 3 qno 4
+// var visitorName = "Furi";
+// var product = "T-shirt";
+// var quantity = 5
+// document.write(visitorName +" ordered "+quantity+" "+product+"(s)"+" on xyz store ")
+
+                //chapter 4
+
+// Javascript assignment chapter 4 qno 1
+// var variable1 = "hello",
+//     variable2 = "furi",
+//     variable3 = "azeem"
+
+// Javascript assignment chapter 4 qno 2
+ // legal variables
+ //1)userName,2)userName23,3)user$Name,4)user_Name,5)$userName.
+ // illegal variables
+ //1)23userName,2)alert,3)user*Name,4)user-Name,5)%userName.
+
+ // Javascript assignment chapter 4 qno 3
+ //a
+//  var my_first$variable = "my_first$variable";
+//  var $userName = "$userName";
+//  var userName23 = "userName23";
+//     var userName = "userName"
+//  document.write("Variable names can only contain"+" "+my_first$variable+","
+//  +$userName+","+userName23+","+userName)
+//b
+// var data1 = "_user"
+// var data2 = "$user"
+// var data3 = "user"
+// document.write("Variables must begin with a "+ data1+","+data2+" or "+data3)
+
+//c
+// var b = "Variable names are case"
+// var c = "sensitive";
+// document.write(b+" "+c)
+
+//d
+// var d = "Variable names should not be JS "
+// var e = "reserved words"
+// document.write(d+e) 
+
+//                       chapter 5-7
+// Javascript assignment  qno 1
+// var num1 = 5;
+// var num2 = 5;
+// var result = num1 + num2;
+// alert("sum of"+" "+num1 + " and "+num2 + " is "+result)
+
+// Javascript assignment  qno 2
+// var num1 = 5;
+// var num2 = 5;
+// var result = num1 - num2;
+// alert("subtration of"+" "+num1 + " and "+num2 + " is "+result)
+
+// var num1 = 5;
+// var num2 = 5;
+// var result = num1 / num2;
+// alert("division of"+" "+num1 + " and "+num2 + " is "+result)
+
+// var num1 = 5;
+// var num2 = 5;
+// var result = num1 * num2;
+// alert("multiplication of"+" "+num1 + " and "+num2 + " is "+result)
+
+// var num1 = 5;
+// var num2 = 5;
+// var result = num1 % num2;
+// alert("modulus of"+" "+num1 + " and "+num2 + " is "+result)
+
+// Javascript assignment  qno 3
+// var abc = 
+// document.write("Value after variable declaration is: "+abc);
+// var num = 3;
+// document.write("<br>");
+// document.write("\n"+"Initial value: "+num)
+// ++num;
+// document.write("<br>");
+// document.write("\n"+"Value after increment is: "+num) 
+// var add = num + 7;
+// document.write("<br>");
+// document.write("\n"+"Value after addition is: "+add)
+// --add
+// document.write("<br>");
+// document.write("\n"+"Value after decrement is: "+add);
+// var reminder = add % 3 ;
+// document.write("<br>");
+// document.write("\n"+"The remainder is : "+ reminder);
+
+// Javascript assignment  qno 4
+// var ticketPrice = 600;
+// var mul = ticketPrice * 5;
+// document.write("Cost of 5 movie ticket is " + mul + " PKR.")
+
+// Javascript assignment qno 5
+// var num = 4
+// var num1 = 1; 
+// document.write("4 "+ "X " + num1 + " = " + num);
+// ++num + ++num + ++num
+// document.write("<br>");
+// document.write("\n 4 "+ "X " + ++num1 + " = " +  ++num)
+// ++num + ++num + ++num
+// document.write("<br>");
+// document.write("\n 4 "+ "X " + ++num1 + " = " +  ++num)
+// ++num + ++num + ++num
+// document.write("<br>");
+// document.write("\n 4 "+ "X " + ++num1 + " = " +  ++num)
+// ++num + ++num + ++num
+// document.write("<br>");
+// document.write("\n 4 "+ "X " + ++num1 + " = " +  ++num)
+// ++num + ++num + ++num
+// document.write("<br>");
+// document.write("\n 4 "+ "X " + ++num1 + " = " +  ++num)
+// ++num + ++num + ++num
+// document.write("<br>");
+// document.write("\n 4 "+ "X " + ++num1 + " = " +  ++num)
+// ++num + ++num + ++num
+// document.write("<br>");
+// document.write("\n 4 "+ "X " + ++num1 + " = " +  ++num)
+// ++num + ++num + ++num
+// document.write("<br>");
+// document.write("\n 4 "+ "X " + ++num1 + " = " +  ++num)
+// ++num + ++num + ++num
+// document.write("<br>");
+// document.write("\n 4 "+ "X " + ++num1 + " = " +  ++num)
+
+
+// Javascript assignment  qno 6
+// var celTemp  =  12;
+// var fehForm = (celTemp*9/5)+32;
+// document.write(celTemp+"C is " + fehForm+"F")
+// document.write("<br>")
+// var fehTemp = 68;
+// var celForm = (fehTemp-32)* 5/9;
+// document.write(fehTemp+"F is " + celForm+"C")
+
+// Javascript assignment  qno 7
+// var PriceItem1 = 560;
+// var noOFItem1 = 2;
+// var PriceItem2 = 200;
+// var noOFItem2 = 5;
+// var charges = 150;
+// var res1 = 560*2
+// var res2 = 200*5
+// var finalRes = res1 + res2 + charges;
+
+// document.write("<h2>"+ "Shopping" +"</h2>")
+// document.write("<br>")
+// document.write("<br>")
+// document.write("<br>")
+// document.write("<br>")
+
+// document.write("Price of Item 1 is "+ PriceItem1)
+// document.write("<br>")
+// document.write("Quantity of Item 1 is "+ noOFItem1);
+// document.write("<br>")
+// document.write("Price of Item 2 is "+ PriceItem2)
+// document.write("<br>")
+// document.write("Quantity of Item 2 is "+ noOFItem2);
+// document.write("<br>")
+// document.write("<br>")
+// document.write("shipping Charges is "+ charges);
+// document.write("<br>")
+// document.write("<br>")
+// document.write("<br>")
+// document.write("Total cost of your order is "+ finalRes + " PKR ") ;
+
+// Javascript assignment  qno 8
+// document.write("<h2>"+ "Marksheet" +"</h2>")
+// document.write("<br>")
+//  document.write("<br>")
+//  document.write("<br>")
+//  var tot = 1200;
+//  var obt = 900;
+// var per = (obt/tot)*100;
+// document.write(
+//     "Total Marks is " + tot
+// )
+
+// document.write("<br>")
+
+// document.write(
+//     "Obtained Marks is " + obt
+// )
+
+// document.write("<br>");
+
+// document.write("your percentage is " + per + "%")
+
+                      // Javascript assignment  qno 9
+
+// var $ = 10;
+// var saudiRiyal = 25;
+// var $ToPkr = $*104;
+// var saudiRiyalToPkr = saudiRiyal*28
+// var add = $ToPkr + saudiRiyalToPkr
+// document.write("<h2>"+"Currency In Pakistan"+"</h2>")
+// document.write("Total Currency In PKR is : "+add)
+
+                          // Javascript assignment  qno 10
+
+// var num = 5+5*10/2;
+// alert(num)
+
+                           // Javascript assignment  qno 11
+
+
+// var currentYear = 2023;
+// var birthYear = 1999
+// document.write("<h2>"+"The Age Calculator"+"</h2>")
+// document.write("The Current Year is " + currentYear);
+// document.write("<br>");
+// document.write("The birth Year is " + birthYear);
+// document.write("<br>");
+// document.write("<br>");
+// document.write("<br>");
+// document.write("This is either 23 or " + (currentYear-birthYear) +" years old");
+
+    // Javascript assignment  qno 12
+
+    // var radius = 12;
+    // var circumference = 2*3.142*radius;
+    // var area = 3.142*(radius*radius);
+   
+
+    // document.write("<h2>"+"The Geomatrier"+"</h2>")
+    // document.write("The Radius is "+ radius)
+    // document.write("<br>")
+    // document.write("the circumference is "+circumference);
+    // document.write("<br>")
+    // document.write("The area is "+area);
+
+      // Javascript assignment  qno 13
+    //   document.write("<h2>"+ "Result" +"</h2>")
+    //   var a = 10;
+    
+    //   document.write("the value of a is "+a);
+    //   document.write("<br>")
+    //   document.write(".......................................................");
+    //  document.write("<br>")
+    //   document.write("<br>")
+    //   document.write("The value of a++ is "+ ++a)
+    //   document.write("<br>")
+    //   document.write("now the value of a is "+a);
+    //  document.write("<br>");
+    //  document.write("<br>");
+    //  document.write("The value of a++ is "+a++)
+    //  document.write("<br>");
+    //  document.write("now the value of a is "+a);
+    //  document.write("<br>");
+    //  document.write("<br>");
+    //  document.write("The value of --a is "+ --a)
+    //  document.write("<br>")
+    //  document.write("now the value of a is "+a);
+    //  document.write("<br>");
+    //  document.write("<br>");
+    //  document.write("The value of a-- is "+ a--)
+    //  document.write("<br>");
+    //  document.write("now the value of a is "+a);
+
+      // Javascript assignment  qno 14
+
+
+    //   var a = 2;
+    //   var b = 1;
+    // var result =  --a;
+    //   //1 
+    //   --a - --b;
+    //   //0 - 0
+    //   --a - --b + ++b;
+    // // -1 - -1   + 0 = -2
+    //   --a - --b + ++b + b--
+    // // -2 - -1  +  0  + 0
+    // console.log(result)
+
+    // Javascript assignment  qno 15
+    // var favSnake = "Oreo";
+    // var currentAge = 17;
+    // var maxAge = 85;
+    // var amouPerDay = 2;
+    // var cal = maxAge-currentAge;
+    // var cal1 = cal*2
+    // document.write("you will need "+cal1+" "+ favSnake+" to last you untill the ripe old age of "+ maxAge)
+
+//               chapter 8-11
+
+   // Javascript assignment  qno 1
+  //  var userName = prompt("Enter your name")
+  //  alert("hi "+userName)
+
+  // Javascript assignment  qno 2
+// var userInput = +prompt("Enter your number");
+// var num = 1
+// document.write(userInput + " X "+num+" = "+userInput)
+// document.write("<br>")
+// document.write(userInput + " X "+ ++num +" = "+userInput*num)
+// document.write("<br>")
+// document.write(userInput + " X "+ ++num +" = "+userInput*num)
+// document.write("<br>")
+// document.write(userInput + " X "+ ++num +" = "+userInput*num)
+// document.write("<br>")
+// document.write(userInput + " X "+ ++num +" = "+userInput*num)
+// document.write("<br>")
+// document.write(userInput + " X "+ ++num +" = "+userInput*num)
+// document.write("<br>")
+// document.write(userInput + " X "+ ++num +" = "+userInput*num)
+// document.write("<br>")
+// document.write(userInput + " X "+ ++num +" = "+userInput*num)
+// document.write("<br>")
+// document.write(userInput + " X "+ ++num +" = "+userInput*num)
+// document.write("<br>")
+// document.write(userInput + " X "+ ++num +" = "+userInput*num)
+
+
+// Javascript assignment  qno 3
+// var userInput = prompt("Enter your city name")
+// if(userInput=='karachi'){
+// alert("welcome to the city of light")
+// }else{
+//   alert("welcome to the "+userInput)
+// }
+
+// Javascript assignment  qno 4
+// var gender = prompt('enter your gender');
+// if(gender == 'male'){
+//   alert('good morning sir')
+// }else if(gender == 'female'){
+//   alert("good morning ma'm")
+// } else{
+//   alert("good morning");
+// }
+
+// Javascript assignment  qno 5
+// var color = prompt("Enter Traffic Signal colour")
+// if(color == 'red' || color == 'Red'){
+//   alert('vehicles must stop')
+// }
+// else if(color == "yellow" || color=="Yellow"){
+//   alert('vehicles should get ready to move')
+// }
+// else if(color == "Green" || color=="green"){
+//   alert('vehicles can move now')
+// }
+// else{
+//   alert("invalid color")}
+
+// Javascript assignment  qno 6
+
+// var currentAge = +prompt('Enter your Cureent age')
+// var maxAge = +prompt('Enter your Max age');
+// if(currentAge <=maxAge){
+//   alert("welcome");
+// } 
+
+// Javascript assignment  qno 7
+// var fuel = +prompt("input remaining fuel in car (in litres")
+// if(fuel<=0.25){
+//   alert("please refuel the car");
+// }
+
+// Javascript assignment  qno 8
+// a)
+//var a = 4;
+// if (++a === 5){
+//  alert("given condition for variable a is true");
+// }//answer true
+
+
+//b) 
+//  var b = 82;
+// if (b++ === 83){
+//  alert("given condition for variable b is true");
+// }//answer false
+
+//c)
+//   var c = 12;
+// if (c++ === 13){
+//  alert("condition 1 is true");
+// }
+// if (c === 13){
+//  alert("condition 2 is true");
+// }
+// if (++c < 14){
+//  alert("condition 3 is true");
+// }
+// if(c === 14){
+//  alert("condition 4 is true");
+// } // answer condition 2 & 4 is true
+
+//d)
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+//  alert("The cost equals");
+// } // condition true
+
+//e)
+// if (true){
+//   alert("True");
+//  }
+//  if (false){
+//   alert("False");
+//  } // answer is true
+
+//f)
+//   if("car" < "cat"){
+//   alert("car is smaller than cat");
+//  } //answer is condition false
+
+// Javascript assignment  qno 9
+ 
+var eng = +prompt('enter your english marks');
+var urdu = +prompt('enter your urdu marks');
+var math = +prompt('enter your math marks');
+var tot = eng + urdu + math;
+var percentage = (tot/300*100);
+if(percentage >=80 && percentage  )
